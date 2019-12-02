9d3443ad85eda3cb67c3faa1cd2cc8e2bfffd53c
Jianmu Deng <deng5945@uwlax.edu>
2019-12-02 00:33:18 -0600

    heap sort

--- src/test/Test.java
+++ src/test/Test.java
@@ -6,32 +6,32 @@ public class Test {
 
     public static void main(String[] args) {
         int[] arr = new int[]{1515, 161, 503, 168, 20, 2442, 15, 14168, 4, 3232};
-        System.out.println(Arrays.toString(insertSort(arr)));
+//        System.out.println(Arrays.toString(insertSort(arr)));
 //        System.out.println(Arrays.toString(selectSort(arr)));
 //        System.out.println(Arrays.toString(bubbleSort(arr)));
 //        System.out.println(Arrays.toString(quickSort(arr, 0, arr.length - 1)));
 //        System.out.println(Arrays.toString(mergeSort(arr)));
 //        System.out.println(Arrays.toString(radixSort(arr)));
-
-        System.out.println(binarySearch(insertSort(arr),1416));
+        System.out.println(Arrays.toString(heapSort(arr)));
+//        System.out.println(binarySearch(insertSort(arr), 1416));
 
     }
 
 
-    public static int binarySearch(int[] a,int t){
+    public static int binarySearch(int[] a, int t) {
         int index = -1;
-        if(a==null || a.length<0) return -1;
+        if (a == null || a.length < 0) return -1;
         int low = 0;
         int high = a.length;
-        while(low<high){
-            int mid = (low + high)/2;
-            if(t==a[mid]){
-                index=mid;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (t == a[mid]) {
+                index = mid;
                 break;
-            }else if(t<a[mid]){
-                high = mid-1;
-            }else{
-                low = mid+1;
+            } else if (t < a[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
 
@@ -132,28 +132,60 @@ public class Test {
 
     public static int[] merge(int[] left, int[] right) {
         int[] result = new int[left.length + right.length];
-        int l=0,r=0,k=0;
-        while(l<left.length && r<right.length){
-            if(left[l]<right[r]){
+        int l = 0, r = 0, k = 0;
+        while (l < left.length && r < right.length) {
+            if (left[l] < right[r]) {
                 result[k++] = left[l++];
-            }else{
+            } else {
                 result[k++] = right[r++];
             }
         }
-        while(r<right.length){
+        while (r < right.length) {
             result[k++] = right[r++];
         }
-        while(l<left.length){
+        while (l < left.length) {
             result[k++] = left[l++];
         }
 
         return result;
     }
 
-
     public static int[] radixSort(int[] a) {
 
         return a;
     }
 
+    public static int[] heapSort(int[] a) {
+        if (a == null || a.length <= 0) return a;
+
+        for (int i = a.length / 2 - 1; i >= 0; i--) {
+            adjustHeap(a,i,a.length);
+        }
+        for (int i = a.length-1; i >0 ; i--) {
+            swap(a,0,i);
+            adjustHeap(a,0,i);
+        }
+        return a;
+    }
+
+    public static void adjustHeap(int[] a,int i, int length){
+        int temp = a[i];//当前父节点
+        for(int k = 2*i+1;k<length;k=2*k+1){
+            if(k+1<length && a[k]<a[k+1]){
+                k+=1;
+            }
+            if(temp<a[k]){
+                swap(a,i,k);
+                i=k;//看他的子树是否受到影响
+            }else{
+                break;
+            }
+        }
+    }
+
+    public static void swap(int[] a, int i, int j){
+        a[i] = a[i] + a[j];
+        a[j] = a[i] - a[j];
+        a[i] = a[i] - a[j];
+    }
 }
